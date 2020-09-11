02f782bac41a41f431563b2e6aa1e3a7f1bed2e3
Georges TAVERNE <gtaverne@e3r7p22.42.fr>
2020-09-11 12:40:06 +0200

    Push de tout d'un coup

---
+++ ex08/ft_sort_int_tab.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gtaverne <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/09/11 11:25:23 by gtaverne          #+#    #+#             */
+/*   Updated: 2020/09/11 12:01:42 by gtaverne         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void ft_sort_int_tab(int *tab, int size);
+void my_swap(int *a, int *b);
+
+void	my_swap(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+void	ft_sort_int_tab(int *tab, int size)
+{
+	int len;
+	int iter;
+	int max[2];
+	
+	len = size-1;
+	
+	while (len > 0)
+	{	
+		max[0] = len;
+		max[1] = *(tab + len);
+		iter = len - 1;
+		while (iter >= 0)
+		{
+			if (max[1] < tab[iter])
+			{
+				max[0] = iter;
+				max[1] = tab[iter];
+			}
+			my_swap((tab + len), (tab + max[0]));
+			iter--;
+		}
+		
+		len--;
+	}
+		 
+
+}
